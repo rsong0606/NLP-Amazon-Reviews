cf8449e77599b74be68340abf8ff586a1e0534ac
rsong0606 <58720071+rsong0606@users.noreply.github.com>
2022-06-20 15:38:14 -0400

    added functions and model

---
+++ LDA_model_nlp.py
@@ -0,0 +1,91 @@
+import data_pre_funs
+import nltk
+import emoji
+import gensim
+import spacy
+import pandas as pd
+import re
+import numpy as np
+from nltk.corpus import stopwords
+import string
+from pprint import pprint
+
+df = pd.read_csv('dataset/AllProductReviews.csv')
+print(df.head())
+print(df.columns)
+
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner', 'textcat'])
+
+df['ReviewTitle'] = df['ReviewTitle'].apply(data_pre_funs.remove_emoji_)
+df['ReviewBody'] = df['ReviewBody'].apply(data_pre_funs.remove_emoji_)
+df = df[df['ReviewBody'].str.len() >= 3]
+df['ReviewBody'] = df['ReviewBody'].apply(data_pre_funs.remove_newline)
+df['ReviewBody'] = df['ReviewBody'].apply(data_pre_funs.remove_special_chars)
+nltk.download('stopwords')
+stop = stopwords.words('english')
+df['ReviewBody'] = df['ReviewBody'].apply(
+    lambda words: ' '.join(word.lower() for word in words.split() if word not in stop))
+print(df.head())
+
+texts = df.ReviewBody.values.tolist()
+
+
+def process_words(texts):
+    result = []
+    for t in texts:
+        t = ' '.join(re.findall(r'\b\w[\w\']+\b', t))
+        doc = nlp(t)
+        result.append([token.lemma_.lower() for token in doc])
+    return result
+
+
+processed_text = process_words(texts)
+
+print(processed_text[:1])
+dictionary = gensim.corpora.Dictionary(processed_text)
+print(f'Number of unique tokens: {len(dictionary)}')
+
+corpus = [dictionary.doc2bow(t) for t in processed_text]
+
+num_topics = 5
+
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                            id2word=dictionary,
+                                            num_topics=num_topics,
+                                            random_state=117, update_every=1,
+                                            chunksize=1500,
+                                            passes=5, iterations=10,
+                                            alpha='asymmetric', eta=1 / 100,
+                                            per_word_topics=True)
+
+print(pprint(lda_model.print_topics(num_words=10)))
+
+
+def get_main_topic_df(model, bow, texts):
+    topic_list = []
+    percent_list = []
+    keyword_list = []
+
+    for wc in bow:
+        topic, percent = sorted(model.get_document_topics(wc), key=lambda x: x[1], reverse=True)[0]
+        topic_list.append(topic)
+        percent_list.append(round(percent, 3))
+        keyword_list.append(' '.join(sorted([x[0] for x in model.show_topic(topic)])))
+
+    result_df = pd.concat([pd.Series(topic_list, name='Dominant_topic'),
+                           pd.Series(percent_list, name='Percent'),
+                           pd.Series(texts, name='Processed_text'),
+                           pd.Series(keyword_list, name='Keywords')], axis=1)
+
+    return result_df
+
+
+main_topic_df = get_main_topic_df(lda_model, corpus, processed_text)
+
+print(main_topic_df.head(10))
+
+grouped_topics = main_topic_df.groupby('Dominant_topic')
+grouped_topics.count()['Processed_text']. \
+    plot.bar(rot=0). \
+    set(title=f'Dominant Topic Frequency in the {len(reviews)} Reviews',
+        ylabel='Topic frequency');
---
+++ data_pre_funs.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+import re
+import string
+
+
+def remove_emoji_(text):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+    return clean_text
+
+
+def remove_newline(text):
+    text = text.replace('\n', '')
+    return text
+
+
+def remove_special_chars(text):
+    for punctuation in string.punctuation:
+        text = text.replace(punctuation, '')
+    return text
+
